514a64c16fe22505752673c89f672c197389ad09
Turbo <turbo.work.dev@gmail.com>
2019-02-19 17:33:54 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: 'airbnb-base',
+  root: true,
+  parserOptions: {
+    ecmaVersion: 6,
+  },
+  env: {
+    es6: true,
+  },
+  rules: {
+    'no-console': 'off',
+    'no-throw-literal': 'off',
+    'no-return-await': 'off',
+    'consistent-return': 'warn',
+    'arrow-body-style': ['error', 'as-needed', { 'requireReturnForObjectLiteral': true }],
+  }
+};
